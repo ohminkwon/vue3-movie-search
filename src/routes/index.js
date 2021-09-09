40555257a876671310f87c1c8e6d33250064f43e
ohminkwon <ohminkwon0720@gmail.com>
2021-09-09 22:35:41 +0900

    start project

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from './Home'
+import Movie from './Movie'
+import About from './About'
+
+export default createRouter({
+  //Hash, History 크게 2가지 모드
+  //Hash 모드로 작업 (예시: https://google.com/#/search)
+  history: createWebHashHistory(),
+
+  //pages
+  //https://google.com/
+  routes:[
+    {
+      path:'/',
+      component: Home
+    },
+    {
+      path: '/movie',
+      component: Movie
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
